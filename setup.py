af94adfb9567ebaa7814ab3ef6646853fa50442d
kfk <alessiocivitillo@gmail.com>
2018-02-09 21:48:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'mypackage',
-  packages = ['mypackage'], # this must be the same as the name above
+  packages = ['wplot'], # this must be the same as the name above
   version = '0.1',
-  description = 'A random test lib',
+  description = 'Easy plot wrap around matplotlib',
   author = 'Peter Downs',
-  author_email = 'peterldowns@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  author_email = 'alessiocivitillo@gmail.com',
+  url = 'https://github.com/kfk/wplot',
+  download_url = 'https://github.com/kfk/wplot/archive/0.1.tar.gz',
+  keywords = ['matplotlib', 'plotting', 'plot wrap', 'bar charts'],
   classifiers = [],
 )
